028cda1ff76c4ce6a3358e42b2309530954dc0bb
Aluno - lab/CESJF <aluno@labcesac.net>
2017-02-17 20:16:52 -0300

    Adiciona função para mover personagens

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var tartaruga = {
-	left:0,
-	bottom: 40,
+	left:40,
+	bottom: 0,
 	passo: 10,
 	escorregar: 20,
 };
@@ -18,4 +18,10 @@ function atualizar() {
 	divTartaruga.style.bottom=tartaruga.bottom+"px";
 	divLebre.style.left=lebre.left+"px";
 	divLebre.style.bottom=lebre.bottom+"px";
+}
+function mover(){
+	tartaruga.left += tartaruga.passo;
+	tartaruga.bottom += tartaruga.passo;
+	lebre.left += lebre.passo;
+	lebre.bottom += lebre.passo;
 }
